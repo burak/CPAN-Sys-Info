1f2c6d5e2bd20112f452b278aba3cfa58efd5def
Burak Gürsoy <burak@cpan.org>
2009-12-07 05:11:20 +0200

    more refactoring

--- eg/sysinfo.pl
+++ eg/sysinfo.pl
@@ -1,6 +1,7 @@
 ﻿#!/usr/bin/perl -w
-package sysinfo;
+package Sysinfo;
 use strict;
+use warnings;
 use lib qw(../Sys-Info-Device-BIOS/lib);
 use vars qw( $VERSION );
 use POSIX qw(locale_h);
@@ -9,6 +10,10 @@ use Sys::Info::Constants qw(NEW_PERL);
 use Time::Elapsed qw( elapsed );
 use Number::Format;
 use Text::Table;
+use Carp qw( croak );
+use constant CPUTF8 => 65_001;
+use constant KB     => 1024;
+use constant LAST_ELEMENT => -1;
 
 $VERSION = '0.10';
 
@@ -22,81 +27,40 @@ my $NA   = 'N/A';
 my $NF   = Number::Format->new( THOUSANDS_SEP => q{,}, DECIMAL_POINT => q{.} );
 my %meta = $os->meta;
 my $need_chcp = $os->is_winnt && $ENV{PROMPT};
-
-my @probe;
-
-my %bit = (
-   cpu => $cpu->bitness,
-   os  => $os->bitness,
-);
-map { $bit{$_} ||= '??' } keys %bit;
-
-@probe = eval {(
-   [ "Sys::Info Version"         => Sys::Info->VERSION                     ],
-   [ "Perl Version"              => $i->perl_long                          ],
-   [ "Host Name"                 => $os->host_name                         ],
-   [ "OS Name"                   => $os->name( long => 1, edition => 1 )   ],
-   [ "OS Version"                => $os->version . '.' . $os->build        ],
-   [ "OS Manufacturer"           => $meta{'manufacturer'}        || $NA    ],
-   [ "OS Configuration"          => $os->product_type            || $NA    ],
-   [ "OS Build Type"             => $meta{'build_type'}          || $NA    ],
-   [ "Running on"                => "$bit{cpu}bit CPU & $bit{os}bit OS"    ],
-   [ "Registered Owner"          => $meta{'owner'}               || $NA    ],
-   [ "Registered Organization"   => $meta{'organization'}        || $NA    ],
-   [ "Product ID"                => $meta{'product_id'}          || $NA    ],
-   [ "Original Install Date"     => scalar localtime $meta{'install_date'} ],
-   [ "System Up Time"            => $up                          || $NA    ],
-   [ "System Manufacturer"       => $meta{'system_manufacturer'} || $NA    ],
-   [ "System Model"              => $meta{'system_model'}        || $NA    ],
-   [ "System Type"               => $meta{'system_type'}         || $NA    ],
-   [ "Processor(s)"              => processors()                 || $NA    ],
-   [ "BIOS Version"              => $i->device('bios')->version  || $NA    ],
-   [ "Windows Directory"         => $meta{windows_dir}           || $NA    ],
-   [ "System Directory"          => $meta{system_dir}            || $NA    ],
-   [ "Boot Device"               => $meta{'boot_device'}         || $NA    ],
-   [ "System Locale"             => $LOCALE                      || $NA    ],
-   [ "Input Locale"              => $LOCALE                      || $NA    ],
-   [ "Time Zone"                 => $os->tz                      || $NA    ],
-   [ "Total Physical Memory"     => mb($meta{'physical_memory_total'}    ) ],
-   [ "Available Physical Memory" => mb($meta{'physical_memory_available'}) ],
-   [ "Virtual Memory: Max Size"  => mb($meta{'page_file_total'}          ) ],
-   [ "Virtual Memory: Available" => mb($meta{'page_file_available'}      ) ],
-   [ "Virtual Memory: In Use"    => vm()                                   ],
-   [ "Page File Location(s)"     => $meta{page_file_path}          || $NA  ],
-   [ "Domain"                    => $os->domain_name               || $NA  ],
-   [ "Logon Server"              => $os->logon_server              || $NA  ],
-
-   [ "Windows CD Key"            => $os->cdkey                     || $NA  ],
-   [ "Microsoft Office CD Key"   => ($os->cdkey( office => 1 ))[0] || $NA  ],
-)};
-
-die "Error fetching information: $@" if $@;
-
 my $oldcp;
-if ( $need_chcp ) {
-   chomp($oldcp = (split /:\s?/, qx(chcp))[-1]);
-   system(chcp => 65001, '2>nul', '1>nul') if $oldcp; # try to change it to unicode
-   eval q{ binmode STDOUT, ':utf8' } if NEW_PERL;
-}
+
+run();
 
 END {
-   system(chcp => $oldcp, '2>nul', '1>nul') if $need_chcp && $oldcp;
+   system chcp => $oldcp, '2>nul', '1>nul' if $need_chcp && $oldcp;
 }
 
-my @titles = ( "FIELD\n=====", "VALUE\n=====");
-@titles = ( "", "");
+sub run {
+   my @probe = probe();
 
-my $tb = Text::Table->new( @titles );
+   if ( $need_chcp ) {
+      ## no critic (InputOutput::ProhibitBacktickOperators)
+      chomp($oldcp = (split /:\s?/xms, qx(chcp))[LAST_ELEMENT]);
+      system chcp => CPUTF8, '2>nul', '1>nul' if $oldcp; # try to change it to unicode
+      if ( NEW_PERL ) {
+         my $eok = eval q{ binmode STDOUT, ':utf8'; 1; };
+      }
+   }
+   my @titles = ( "FIELD\n=====", "VALUE\n=====");
+   @titles = ( q{}, q{});
+
+   my $tb = Text::Table->new( @titles );
    $tb->load( @probe );
-print "\n";
-print $tb;
+   print "\n", $tb or croak "Unable to orint to STDOUT: $!";
+   return;
+}
 
 sub processors {
-   my $rv = sprintf "%s ~%sMHz",
+   my $rv = sprintf '%s ~%sMHz',
                     scalar($cpu->identify),
                     $cpu->speed;
-   $rv =~ s{\s+}{ }sg;
-   $rv;
+   $rv =~ s{\s+}{ }xmsg;
+   return $rv;
 }
 
 sub vm {
@@ -107,10 +71,61 @@ sub vm {
 
 sub mb {
    my $kb = shift || return $NA;
-   my $int = sprintf '%.0f', $kb / 1024;
+   my $int = sprintf '%.0f', $kb / KB;
    return sprintf '%s MB', $NF->format_number( $int );
 }
 
+sub probe {
+   my %bit = (
+      cpu => $cpu->bitness,
+      os  => $os->bitness,
+   );
+   $bit{$_} ||= q{??} for keys %bit;
+
+   my @rv = eval {(
+   [ 'Sys::Info Version'         => Sys::Info->VERSION                     ],
+   [ 'Perl Version'              => $i->perl_long                          ],
+   [ 'Host Name'                 => $os->host_name                         ],
+   [ 'OS Name'                   => $os->name( long => 1, edition => 1 )   ],
+   [ 'OS Version'                => $os->version . q{.} . $os->build       ],
+   [ 'OS Manufacturer'           => $meta{'manufacturer'}        || $NA    ],
+   [ 'OS Configuration'          => $os->product_type            || $NA    ],
+   [ 'OS Build Type'             => $meta{'build_type'}          || $NA    ],
+   [ 'Running on'                => "$bit{cpu}bit CPU & $bit{os}bit OS"    ],
+   [ 'Registered Owner'          => $meta{'owner'}               || $NA    ],
+   [ 'Registered Organization'   => $meta{'organization'}        || $NA    ],
+   [ 'Product ID'                => $meta{'product_id'}          || $NA    ],
+   [ 'Original Install Date'     => scalar localtime $meta{'install_date'} ],
+   [ 'System Up Time'            => $up                          || $NA    ],
+   [ 'System Manufacturer'       => $meta{'system_manufacturer'} || $NA    ],
+   [ 'System Model'              => $meta{'system_model'}        || $NA    ],
+   [ 'System Type'               => $meta{'system_type'}         || $NA    ],
+   [ 'Processor(s)'              => processors()                 || $NA    ],
+   [ 'BIOS Version'              => $i->device('bios')->version  || $NA    ],
+   [ 'Windows Directory'         => $meta{windows_dir}           || $NA    ],
+   [ 'System Directory'          => $meta{system_dir}            || $NA    ],
+   [ 'Boot Device'               => $meta{'boot_device'}         || $NA    ],
+   [ 'System Locale'             => $LOCALE                      || $NA    ],
+   [ 'Input Locale'              => $LOCALE                      || $NA    ],
+   [ 'Time Zone'                 => $os->tz                      || $NA    ],
+   [ 'Total Physical Memory'     => mb($meta{'physical_memory_total'}    ) ],
+   [ 'Available Physical Memory' => mb($meta{'physical_memory_available'}) ],
+   [ 'Virtual Memory: Max Size'  => mb($meta{'page_file_total'}          ) ],
+   [ 'Virtual Memory: Available' => mb($meta{'page_file_available'}      ) ],
+   [ 'Virtual Memory: In Use'    => vm()                                   ],
+   [ 'Page File Location(s)'     => $meta{page_file_path}          || $NA  ],
+   [ 'Domain'                    => $os->domain_name               || $NA  ],
+   [ 'Logon Server'              => $os->logon_server              || $NA  ],
+
+   [ 'Windows CD Key'            => $os->cdkey                     || $NA  ],
+   [ 'Microsoft Office CD Key'   => ($os->cdkey( office => 1 ))[0] || $NA  ],
+   )};
+
+   croak "Error fetching information: $@" if $@;
+   return @rv;
+}
+
+1;
 
 __END__
 
