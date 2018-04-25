6832b8e0cf1e1457fc4563bb0e363bf4e639a5b7
Burak Gursoy <burak@cpan.org>
2018-04-25 16:13:29 +0200

    Use dzil

---
+++ check_os.pl
@@ -0,0 +1,25 @@
+
+# BEGIN dynamic
+
+# This section is used/injected by dzil and not to be executed as a
+# standalone program
+
+use strict;
+# copy-paste from Sys::Info::Constants
+BEGIN {
+    if ( ! defined &OSID ) {
+        my %OS = (
+            MSWin32  => 'Windows',
+            MSWin64  => 'Windows',
+            linux    => 'Linux',
+            darwin   => 'OSX',
+        );
+        $OS{$_} = 'BSD' for qw( freebsd openbsd netbsd );
+        my $ID = $OS{ $^O } || 'Unknown';
+        *OSID = sub () { "$ID" }
+    }
+}
+
+requires( 'Sys::Info::Driver::' . OSID() => '0.78');
+
+# END dynamic
